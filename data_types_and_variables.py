6f39ea22cf2c643dcda37183e78a5990d6e5a36d
Chellyann M. Moreno <Chellyannmoreno@gmail.com>
2023-03-17 15:24:12 -0500

    python exercises

---
+++ data_types_and_variables.py
@@ -0,0 +1,9 @@
+# 5. You have rented some movies for your kids:
+
+# The Little Mermaid for 3 days
+# Brother Bear for 5 days
+# Hercules for 1 day
+# If the daily fee to rent a movie is 3 dollars, how much will you have to pay?
+print(3*3)+(3*1)+ (5*3)
+
+
